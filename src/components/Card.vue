3279d8f2c910af38eab6512c5b7ce4a08a57313c
Takumaron <tennis.takumax@gmail.com>
2019-02-02 18:00:30 +0900

    コンポーネント格納ディレクトリの追加とカード，ヘッダーコンポーネントの作成

---
+++ src/components/Card.vue
@@ -0,0 +1,50 @@
+<template>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-4" v-for="card in cards">
+        <div class="card w-100 my-3">
+          <img
+            class="card-img-top img-fluid"
+            src="https://placehold.jp/300x200.png"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h2 class="card-title">{{ card.name }}</h2>
+            <h3>{{ card.university }}</h3>
+            <h4>{{ card.prefecture }}</h4>
+            <p class="card-text">{{ card.overview }}</p>
+            <a href="#!" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+var GET_URL = "http://localhost:3000/labs/1";
+
+export default {
+  name: "Card",
+  data() {
+    return {
+      cards: [],
+      json_data: ""
+    };
+  },
+  created() {
+    axios
+      .get(GET_URL)
+      .then(response => {
+        this.json_data = response.data;
+        this.cards.push(response.data);
+      })
+      .catch(err => {
+        console.log("err:", err);
+      });
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost:8080/lab"> 研究室 </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost:8080/user"> ユーザ </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost:8080/login">
+              ログイン
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost:8080/userregister">
+              ユーザ登録
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://localhost:8080/labregister">
+              研究室登録
+            </a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input
+            class="form-control mr-sm-2"
+            type="text"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped></style>
