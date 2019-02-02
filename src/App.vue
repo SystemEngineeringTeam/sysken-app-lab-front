0aa73a50019264050091a71ab0c03f1d738eec59
Takumaron <tennis.takumax@gmail.com>
2019-02-02 18:00:30 +0900

    コンポーネント格納ディレクトリの追加とカード，ヘッダーコンポーネントの作成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <div id="app" class="h-100">
-    <router-view/>
-  </div>
+  <div id="app" class="h-100"><router-view /></div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    component: {}
-  }
+import Header from "./components/Header.vue";
+export default {
+  name: "App",
+  components: {}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/pages/Lab.vue
+++ src/pages/Lab.vue
@@ -1,61 +1,64 @@
 <template>
   <div id="lab">
-    <h1>{{ json_data }}</h1>
-    <hr>
-    <ul>
-      <li>研究室名：{{ name }}</li>
-      <li>大学名：{{ university }}</li>
-      <li>県名：{{ prefecture }}</li>
-      <li>オーバービュー：{{ overview }}</li>
-    </ul>
+    <Header></Header>
+    <card></card>
     <router-link to="/user">Go to user</router-link>
     <router-link to="/login">Go to login</router-link>
   </div>
 </template>
 
 <script>
+import Card from "../components/Card.vue";
+import Header from "../components/Header.vue";
 
-  var GET_URL = "http://localhost:3000/labs/1";
+var GET_URL = "http://localhost:3000/labs/1";
 
-  export default {
-    el: '#lab',
-    name: 'lab',
-    data() {
-      return {
-        json_data: '',
-        name: '',
-        university: '',
-        prefecture: '',
-        overview: '',
-
-      }
-    },
-    created() {
-      axios.get(GET_URL)
-        .then(response => {
-          this.json_data = response.data;
-          this.name = response.data.name;
-          this.university = response.data.university;
-          this.prefecture = response.data.prefecture;
-          this.overview = response.data.overview;
-        }).catch(err => {
-        console.log('err:', err);
+export default {
+  el: "#lab",
+  name: "lab",
+  components: {
+    Card,
+    Header
+  },
+  data() {
+    return {
+      json_data: "",
+      name: "",
+      university: "",
+      prefecture: "",
+      overview: ""
+    };
+  },
+  created() {
+    axios
+      .get(GET_URL)
+      .then(response => {
+        this.json_data = response.data;
+        this.name = response.data.name;
+        this.university = response.data.university;
+        this.prefecture = response.data.prefecture;
+        this.overview = response.data.overview;
+      })
+      .catch(err => {
+        console.log("err:", err);
       });
 
-
-      axios.get(GET_URL, {
+    axios
+      .get(GET_URL, {
         withCredentials: true
-      }).then(response => {
-        console.log('body:', response.data);     // response body.
-      }).catch(err => {
-        console.log('err:', err);
+      })
+      .then(response => {
+        console.log("body:", response.data); // response body.
+      })
+      .catch(err => {
+        console.log("err:", err);
       });
-    }
   }
+};
 </script>
 
 <style>
-  a {
-    margin: 20px 20px;
-  }
+a {
+  margin: 20px 20px;
+}
 </style>
