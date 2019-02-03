936a97531f1592e6888af17a4b7f7db0d30c9d49
Takumaron <tennis.takumax@gmail.com>
2019-02-03 11:59:43 +0900

    ユーザと研究室用それぞれのカードコンポーネントを作成
    ユーザ表示画面も研究室同様バックエンドとの連携ずみ

---
+++ src/components/UserCard.vue
@@ -0,0 +1,52 @@
+<template>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-4" v-for="card in cards">
+        <div class="card w-100 my-3">
+          <img
+            class="card-img-top img-fluid"
+            src="https://placehold.jp/300x200.png"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h2 class="card-title">{{ card.name }}</h2>
+            <h3>{{ card.gender }}</h3>
+            <h4>{{ card.grade }}</h4>
+            <p class="card-text">{{ card.lab.name }}</p>
+            <p class="card-text">{{ card.lab.overview }}</p>
+            <p class="card-text">{{ card.context }}</p>
+            <a href="#!" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+var GET_URL = "http://localhost:3000/users/1";
+
+export default {
+  name: "Card",
+  data() {
+    return {
+      cards: [],
+      json_data: ""
+    };
+  },
+  created() {
+    axios
+      .get(GET_URL)
+      .then(response => {
+        this.json_data = response.data;
+        this.cards.push(response.data);
+      })
+      .catch(err => {
+        console.log("err:", err);
+      });
+  }
+};
+</script>
+
+<style scoped></style>
