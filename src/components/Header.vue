90415f5028231f6259ddcc5cfc578107cc43ee80
Takumaron <tennis.takumax@gmail.com>
2019-03-16 19:33:55 +0900

    fix:homepage /usersへ変更

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,7 +22,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/users/">
+            <a class="nav-link" href="/users">
               ユーザ一覧
             </a>
           </li>
