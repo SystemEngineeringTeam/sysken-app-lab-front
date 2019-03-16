a13acc298488a881f1b2fe446156fcc63deaef5b
Takumaron <tennis.takumax@gmail.com>
2019-03-16 19:17:56 +0900

    new:component 検索・絞込み機能の雛形追加

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <Header></Header>
     <Carousel></Carousel>
+    <search></search>
     <section>
       <h1 class="text-center my-5">- 研究室一覧 -</h1>
       <LabCard></LabCard>
@@ -13,13 +14,15 @@
 import Header from "../components/Header";
 import Carousel from "../components/Carousel";
 import LabCard from "../components/LabCard";
+import Search from "../components/SearchEngine"
 
 export default {
   name: "Home",
   components: {
     Header,
     Carousel,
-    LabCard
+    LabCard,
+    Search
   }
 };
 </script>
