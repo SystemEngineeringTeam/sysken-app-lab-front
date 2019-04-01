0c0e2c8b6e536ae636c0ef3dccb412a7d7c19d6c
Takumaron <tennis.takumax@gmail.com>
2019-04-01 22:55:58 +0900

    update:0001 研究室名等の情報とSNSリンクを配置

--- src/components/form_group/laboratory/profile.vue
+++ src/components/form_group/laboratory/profile.vue
@@ -2,7 +2,7 @@
   <div class="card">
     <div class="profile-imgs position-relative">
       <div class="position-relative">
-        <label for="img-selector1"
+        <label class="w-100" for="img-selector1"
           ><input
             @change="choosedCover($event)"
             id="img-selector1"
@@ -12,20 +12,39 @@
             v-if="imageData[0]"
         /></label>
       </div>
-      <div>
-        <label for="img-selector2"
-          ><input
-            @change="choosedLogo($event)"
-            id="img-selector2"
-            type="file"/><img
-            class="profile-imgs-icon position-absolute rounded-lg img-thumbnail col-5 col-md-3"
-            :src="imageData[1]"
-            v-if="imageData[1]"
-        /></label>
-      </div>
+      <label class="w-100" for="img-selector2"
+        ><input
+          @change="choosedLogo($event)"
+          id="img-selector2"
+          type="file"/><img
+          class="profile-imgs-icon position-absolute rounded-lg img-thumbnail col-5 col-md-3"
+          :src="imageData[1]"
+          v-if="imageData[1]"
+      /></label>
     </div>
-    <div class="card-body">
-      <div class="preview"></div>
+    <div class="card-body row justify-content-end">
+      <div class="preview col-7 col-md-8 col-lg-8 col-sm-10">
+        <h1>システム工学研究会</h1>
+        <!-- //TODO: 大学HPへの遷移と，TOPページにて検索（県名で） -->
+        <h2><a href="#">愛知工業大学</a>(<a href="#">愛知県</a>)</h2>
+        <div class="mb-5 contact">
+          <a class="text-decoration-none" v-if="sns.twitter" :href="sns.twitter"
+            ><i class="fab fa-twitter-square"></i
+          ></a>
+          <a
+            class="text-decoration-none"
+            v-if="sns.facebook"
+            :href="sns.facebook"
+            ><i class="fab fa-facebook-square"></i
+          ></a>
+          <a
+            class="text-decoration-none"
+            v-if="sns.instagram"
+            :href="sns.instagram"
+            ><i class="fab fa-instagram"></i
+          ></a>
+        </div>
+      </div>
       <div class="edit"><form method="POST"></form></div>
     </div>
   </div>
@@ -37,9 +56,15 @@ export default {
   data() {
     return {
       imageData: [
-        require("../../../../src/assets/image/coverTest.jpg").toString(),
-        require("../../../../src/assets/image/labRegister.svg").toString()
-      ]
+        require("../../../../src/assets/image/coverTest.jpg"),
+        require("../../../../src/assets/image/labRegister.svg")
+      ],
+      sns: {
+        // 受け取ったSNSのリンクが文字列として入る．リンクが存在していればv-if=trueとなる
+        twitter: "https://twitter.com",
+        facebook: "",
+        instagram: ""
+      }
     };
   },
   methods: {
@@ -74,20 +99,32 @@ export default {
 </script>
 
 <style scoped>
+.preview {
+  margin: -40px 0;
+}
+i {
+  font-size: 5vw;
+  margin: 1vw;
+}
+h1 {
+  font-size: 3vw;
+}
+h2 {
+  font-size: 2vw;
+}
 label {
-  z-index: 10;
   cursor: pointer;
 }
 label > input {
   display: none;
 }
 .profile-imgs > div img {
-  max-height: 300px;
+  max-height: 50vh;
   object-fit: cover;
 }
 .profile-imgs-icon {
   max-height: 200px;
-  bottom: -20%;
+  bottom: -8vw;
   left: 3%;
 }
 .profile-imgs-icon:hover,
